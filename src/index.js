76a94f27a27b09c879a73c755c15a8ca9c6a33d5
ivanichiki <62132967+ivanichiki@users.noreply.github.com>
2020-05-13 11:56:50 +0300

    Finish base framework functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import './module.js'
-import './scss/index.scss'
-console.log('Hi sasd')
+import './scss/index.scss';
+import {Excel} from './components/excel/Excel';
+import {Formula} from './components/fornula/Formula';
+import {Toolbar} from './components/toolbar/Toolbar';
+import {Header} from './components/header/Header';
+import {Table} from './components/table/Table';
+
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+});
+
+excel.render()
