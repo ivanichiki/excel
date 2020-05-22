81849d0f659a7be51f01227dcf07dc5d5c46e1ed
ivanichiki <62132967+ivanichiki@users.noreply.github.com>
2020-05-22 09:41:00 +0300

    Finish base routing

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,37 @@
+import {Page} from '../core/Page';
+import {$} from '../core/dom';
+
+export class DashboardPage extends Page {
+  getRoot() {
+    return $.create('div', 'db').html(`
+     <div class="db">
+      <div class="db__header">
+        <h1>Excel Dashboard</h1>
+      </div>
+      <div class="db__new">
+        <div class="db__view">
+          <a href="#" class="db__create">
+            Новая <br> таблица
+          </a>
+        </div>
+      </div>
+      <div class="db__table db__view">
+        <div class="db__list-header">
+          <span>Название</span>
+          <span>Дата открытия</span>
+        </div>
+        <div class="db__list">
+          <li class="db__record">
+            <a href="#">Таблица №1</a>
+            <strong>12.06.2020</strong>
+          </li>
+          <li class="db__record">
+            <a href="#">Таблица №2</a>
+            <strong>12.06.2020</strong>
+          </li>
+        </div>
+      </div>
+    </div>
+    `)
+  }
+}
